d5db1e0fcbd345d3ef43db685faaf81f482d5efa
Cyberhaxker <muchenjeharold@gmail.com>
2025-12-25 01:38:58 +0200

    feat: Dockerising

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+    usePolling: true,
+    },
+  },
   base: "/Harold-port/",
 })
